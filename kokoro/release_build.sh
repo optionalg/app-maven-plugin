c1b7874383369b83bc5825d7bd173327ae76fbcf
Qingyang Chen <qingyangc@google.com>
2017-09-21 15:04:34 -0400

    Testing fetching signed artifacts.

--- kokoro/release_build.sh
+++ kokoro/release_build.sh
@@ -5,14 +5,13 @@ set -e
 # Display commands to stderr.
 set -x
 
-sudo /opt/google-cloud-sdk/bin/gcloud components update
-sudo /opt/google-cloud-sdk/bin/gcloud components install app-engine-java
-
 cd github/app-maven-plugin
-./mvnw -Prelease -B -U verify
 
 # copy pom with the name expected in the Maven repository
 ARTIFACT_ID=$(mvn -B help:evaluate -Dexpression=project.artifactId 2>/dev/null | grep -v "^\[")
 PROJECT_VERSION=$(mvn -B help:evaluate -Dexpression=project.version 2>/dev/null| grep -v "^\[")
-cp pom.xml target/${ARTIFACT_ID}-${PROJECT_VERSION}.pom
+
+
+echo "ARTIFACT_ID=$ARTIFACT_ID"
+echo "PROJECT_VERSION=$PROJECT_VERSION"
 
