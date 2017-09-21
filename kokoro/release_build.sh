d177f3fb207449bc226749da182bd7de18cd7811
Qingyang Chen <qingyangc@google.com>
2017-09-21 15:21:56 -0400

    Testing fetching signed artifacts.

--- kokoro/release_build.sh
+++ kokoro/release_build.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
-# Fail on any error.
+# Fails on any error.
 set -e
-# Display commands to stderr.
+# Displays commands to stderr.
 set -x
 
 cd github/app-maven-plugin
 
-# copy pom with the name expected in the Maven repository
 ARTIFACT_ID=$(mvn -B help:evaluate -Dexpression=project.artifactId 2>/dev/null | grep -v "^\[")
 PROJECT_VERSION=$(mvn -B help:evaluate -Dexpression=project.version 2>/dev/null| grep -v "^\[")
 
 
 echo "ARTIFACT_ID=$ARTIFACT_ID"
 echo "PROJECT_VERSION=$PROJECT_VERSION"
+echo "$KOKORO_GFILE_DIR"
+
+cd $KOKORO_GFILE_DIR
+
+# Finds the latest directory under prod/app-maven-plugin/ubuntu/release/.
+LAST_BUILD=$(ls prod/app-maven-plugin/ubuntu/release/ | sort -rV | head -1)
+
+echo "LAST_BUILD=$LAST_BUILD"
+
+# Finds the bundled jars in the latest signed artifact directory.
+FILES=$(find `pwd`/prod/app-maven-plugin/gcp_ubuntu/release/${LAST_BUILD}/* -type f \( -iname \*-bundle.jar \))
+
+echo "FILES=$FILES"
+
+"appengine-maven-plugin-1.3.2-SNAPSHOT-bundle.jar"
 
