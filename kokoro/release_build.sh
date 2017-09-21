a9f613b899998f3f52ea94fdbd1b72aad6658248
Qingyang Chen <qingyangc@google.com>
2017-09-21 15:43:41 -0400

    Testing fetching signed artifacts.

--- kokoro/release_build.sh
+++ kokoro/release_build.sh
@@ -5,6 +5,40 @@ set -e
 # Display commands to stderr.
 set -x
 
+
+### REMOVE THIS
+echo "Publishing test!"
+# TODO: Implement publishing to Sonatype.
+
+cd github/app-maven-plugin
+
+ARTIFACT_ID=$(mvn -B help:evaluate -Dexpression=project.artifactId 2>/dev/null | grep -v "^\[")
+PROJECT_VERSION=$(mvn -B help:evaluate -Dexpression=project.version 2>/dev/null| grep -v "^\[")
+
+
+echo "ARTIFACT_ID=$ARTIFACT_ID"
+echo "PROJECT_VERSION=$PROJECT_VERSION"
+echo "$KOKORO_GFILE_DIR"
+
+cd ../../$KOKORO_GFILE_DIR
+
+# Finds the latest directory under prod/app-maven-plugin/ubuntu/release/.
+LAST_BUILD=$(ls prod/app-maven-plugin/ubuntu/release/ | sort -rV | head -1)
+
+echo "LAST_BUILD=$LAST_BUILD"
+
+# Finds the bundled jars in the latest signed artifact directory.
+FILES=$(find `pwd`/prod/app-maven-plugin/gcp_ubuntu/release/${LAST_BUILD}/* -type f \( -iname \*-bundle.jar \))
+
+echo "FILES=$FILES"
+
+"appengine-maven-plugin-1.3.2-SNAPSHOT-bundle.jar"
+
+exit 0
+### END REMOVE THIS
+
+
+
 sudo /opt/google-cloud-sdk/bin/gcloud components update
 sudo /opt/google-cloud-sdk/bin/gcloud components install app-engine-java
 
