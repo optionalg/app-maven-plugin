ba2c1da63a4ddddd8257440f205873829e70ff7b
Qingyang Chen <qingyangc@google.com>
2017-09-21 15:29:20 -0400

    Testing fetching signed artifacts.

--- kokoro/release_build.sh
+++ kokoro/release_build.sh
@@ -1,31 +1,18 @@
 #!/bin/bash
 
-# Fails on any error.
+# Fail on any error.
 set -e
-# Displays commands to stderr.
+# Display commands to stderr.
 set -x
 
-cd github/app-maven-plugin
+sudo /opt/google-cloud-sdk/bin/gcloud components update
+sudo /opt/google-cloud-sdk/bin/gcloud components install app-engine-java
 
+cd github/app-maven-plugin
+./mvnw -Prelease -B -U verify
+
+# copy pom with the name expected in the Maven repository
 ARTIFACT_ID=$(mvn -B help:evaluate -Dexpression=project.artifactId 2>/dev/null | grep -v "^\[")
 PROJECT_VERSION=$(mvn -B help:evaluate -Dexpression=project.version 2>/dev/null| grep -v "^\[")
-
-
-echo "ARTIFACT_ID=$ARTIFACT_ID"
-echo "PROJECT_VERSION=$PROJECT_VERSION"
-echo "$KOKORO_GFILE_DIR"
-
-cd $KOKORO_GFILE_DIR
-
-# Finds the latest directory under prod/app-maven-plugin/ubuntu/release/.
-LAST_BUILD=$(ls prod/app-maven-plugin/ubuntu/release/ | sort -rV | head -1)
-
-echo "LAST_BUILD=$LAST_BUILD"
-
-# Finds the bundled jars in the latest signed artifact directory.
-FILES=$(find `pwd`/prod/app-maven-plugin/gcp_ubuntu/release/${LAST_BUILD}/* -type f \( -iname \*-bundle.jar \))
-
-echo "FILES=$FILES"
-
-"appengine-maven-plugin-1.3.2-SNAPSHOT-bundle.jar"
+cp pom.xml target/${ARTIFACT_ID}-${PROJECT_VERSION}.pom
 
--- kokoro/release_publish.sh
+++ kokoro/release_publish.sh
@@ -7,3 +7,27 @@ set -x
 
 echo "Publishing test!"
 # TODO: Implement publishing to Sonatype.
+
+cd github/app-maven-plugin
+
+ARTIFACT_ID=$(mvn -B help:evaluate -Dexpression=project.artifactId 2>/dev/null | grep -v "^\[")
+PROJECT_VERSION=$(mvn -B help:evaluate -Dexpression=project.version 2>/dev/null| grep -v "^\[")
+
+
+echo "ARTIFACT_ID=$ARTIFACT_ID"
+echo "PROJECT_VERSION=$PROJECT_VERSION"
+echo "$KOKORO_GFILE_DIR"
+
+cd ../../$KOKORO_GFILE_DIR
+
+# Finds the latest directory under prod/app-maven-plugin/ubuntu/release/.
+LAST_BUILD=$(ls prod/app-maven-plugin/ubuntu/release/ | sort -rV | head -1)
+
+echo "LAST_BUILD=$LAST_BUILD"
+
+# Finds the bundled jars in the latest signed artifact directory.
+FILES=$(find `pwd`/prod/app-maven-plugin/gcp_ubuntu/release/${LAST_BUILD}/* -type f \( -iname \*-bundle.jar \))
+
+echo "FILES=$FILES"
+
+"appengine-maven-plugin-1.3.2-SNAPSHOT-bundle.jar"
