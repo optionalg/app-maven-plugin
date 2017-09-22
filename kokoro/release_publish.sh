b865b0e4b8ef1ae46ec2cf422ef22e25806f2b79
Qingyang Chen <qingyangc@google.com>
2017-09-22 11:15:05 -0400

    Testing fetching signed artifacts.

--- kokoro/release_publish.sh
+++ kokoro/release_publish.sh
@@ -31,7 +31,7 @@ GetSessionID() {
 	local cookies_temp=$(mktemp /tmp/sonatype_cookies.XXXXXXX)
 
 	# Sends a login request.
-	local login_response=$(curl 'https://oss.sonatype.org/service/local/authentication/login' -X 'GET' -u '$credentials' -c $cookies_temp 2> /dev/null)
+	local login_response=$(curl 'https://oss.sonatype.org/service/local/authentication/login' -X 'GET' -u "$credentials" -c $cookies_temp 2> /dev/null)
 
 	# Checks if login was successful.
 	echo $login_response | grep -q '<loggedIn>true</loggedIn>'
