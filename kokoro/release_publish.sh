af7a0003639b0c1f45c8fa3dd325aac163e462ef
Qingyang Chen <qingyangc@google.com>
2017-09-22 10:29:43 -0400

    Testing fetching signed artifacts.

--- kokoro/release_publish.sh
+++ kokoro/release_publish.sh
@@ -5,44 +5,14 @@
 # Display commands to stderr.
 set -x
 
-echo "Publishing test!"
-# TODO: Implement publishing to Sonatype.
-
-# pwd
-# cd github/app-maven-plugin
-
-# ARTIFACT_ID=$(mvn -B help:evaluate -Dexpression=project.artifactId 2>/dev/null | grep -v "^\[")
-# PROJECT_VERSION=$(mvn -B help:evaluate -Dexpression=project.version 2>/dev/null| grep -v "^\[")
-
-
-# echo "ARTIFACT_ID=$ARTIFACT_ID"
-# echo "PROJECT_VERSION=$PROJECT_VERSION"
-
-echo "$SONATYPE_USERNAME"
-echo "$SONATYPE_PASSWORD"
-
-echo "$KOKORO_GFILE_DIR"
-
+# Goes to the GCS directory.
 cd $KOKORO_GFILE_DIR
-
-ls prod
-ls prod/app-maven-plugin
-ls prod/app-maven-plugin/ubuntu
-ls prod/app-maven-plugin/ubuntu/release-sign
 
 # Finds the latest ubuntu/release-sign build directory.
 LAST_BUILD=$(ls prod/app-maven-plugin/ubuntu/release-sign/ | sort -rV | head -1)
 
-echo "LAST_BUILD=$LAST_BUILD"
-
-# Finds the bundled jars in the latest signed artifact directory.
-FILES=$(find `pwd`/prod/app-maven-plugin/ubuntu/release-sign/${LAST_BUILD}/* -type f \( -iname \*-bundle.jar \))
-
-echo "FILES=$FILES"
-
-"appengine-maven-plugin-1.3.2-SNAPSHOT-bundle.jar"
-
-exit 1
+# Finds the bundled jar file in the latest signed artifact directory.
+BUNDLED_JAR_FILE=$(find `pwd`/prod/app-maven-plugin/ubuntu/release-sign/${LAST_BUILD}/* -type f \( -iname \*-bundle.jar \))
 
 # Usage: GetSessionID <username> <password> <variable name>
 # Stores the Nexus session ID in the given variable.
@@ -51,14 +21,13 @@ GetSessionID() {
 	local password=$2
 	local __resultvar=$3
 
-	# Converts the credentials from <username>:<password> form to base64.
 	local credentials="$username:$password"
-	local credentials64=$(echo -n $credentials | base64)
 
+	# Makes a temporary file to store the login cookies.
 	local cookies_temp=$(mktemp /tmp/sonatype_cookies.XXXXXXX)
 
-	# Gets the session ID.
-	local login_response=$(curl 'https://oss.sonatype.org/service/local/authentication/login' -X 'GET' -H "Authorization: Basic $credentials64" -c $cookies_temp 2> /dev/null)
+	# Sends a login request.
+	local login_response=$(curl 'https://oss.sonatype.org/service/local/authentication/login' -X 'GET' -u '$credentials' -c $cookies_temp 2> /dev/null)
 
 	# Checks if login was successful.
 	echo $login_response | grep -q '<loggedIn>true</loggedIn>'
@@ -69,6 +38,7 @@ GetSessionID() {
 		return 1
 	fi
 
+	# Extracts the session ID from the cookies.
 	local nxsessionid
 	local nxsessionid_line=$(cat $cookies_temp | grep 'NXSESSIONID')
 	nxsessionid=$(echo -n $nxsessionid_line | awk '{print $NF}')
@@ -82,13 +52,15 @@ UploadJAR() {
 	curl 'https://oss.sonatype.org/service/local/staging/bundle_upload' -H "Cookie: NXSESSIONID=$1" -H 'Content-Type: multipart/form-data' --compressed -F "file=@$2"
 }
 
-# Get the session ID.
+# Gets the session ID.
 GetSessionID $SONATYPE_USERNAME $SONATYPE_PASSWORD NXSESSIONID
 if [ $? -eq 1 ]; then
 	Die 'Login failed!'
 fi
 echo 'Login successful.'
 
-# Upload the bundled JAR file.
+# Uploads the bundled JAR file.
 echo 'Uploading artifact...'
 # UploadJAR $NXSESSIONID $BUNDLED_JAR_FILE
+
+# TODO: Release on Sonatype.
